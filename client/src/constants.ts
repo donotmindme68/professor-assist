be5aa1c2f9a9a5cb8c3b0480eb4fcdac10530c45
yoni_ash <yonatha12345678910@gmail.com>
2025-02-26 02:28:29 +0400

    Fixes

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://professor-assist-production.up.railway.app/api' //todo: temp fix
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api' //todo: temp fix
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://professor-assist-production.up.railway.app/api' //todo: temp fix
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api' //todo: temp fix
